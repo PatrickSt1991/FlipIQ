2ab9b7cfdccbe5a00f5995e116efd65f0bc6216e
PatrickSt1991 <18744743+PatrickSt1991@users.noreply.github.com>
2026-07-25 21:09:50 +0200

    Bump version to 0.3.3 (eBay folded into engine)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "nl.madebypatrick.flipiq"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.3.2"
+        versionCode = 6
+        versionName = "0.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
