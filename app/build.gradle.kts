ea9285dcee1feb6210e10ffb50f44e01f4df6653
PatrickSt1991 <18744743+PatrickSt1991@users.noreply.github.com>
2026-07-25 19:34:00 +0200

    Bump version to 0.3.1 (eBay proxy build)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "nl.madebypatrick.flipiq"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
